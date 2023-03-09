31de05a34f7587e707083303945557aecb3451b4
Carl Anuszczyk (gradewave 0182) <carl@gradewave.com>
2023-03-08 20:25:19 -0500

    Updating sites for reseller when updating/adding reseller

---
+++ ResellerRepository/Services/UpdateReseller/UpdateResellerAddSitesForEachVendor.cs
@@ -0,0 +1,57 @@
+﻿namespace ResellerRepository;
+
+using CommonServices;
+
+using DatabaseContext;
+
+using Microsoft.EntityFrameworkCore;
+
+using ResellerRepositoryTypes;
+
+public class UpdateResellerAddSitesForEachVendor : IUpdateReseller
+{
+    private IUpdateReseller updateReseller;
+
+    private readonly IDateTimeService dateTimeService;
+
+    public UpdateResellerAddSitesForEachVendor(IUpdateReseller updateReseller, IDateTimeService dateTimeService)
+    {
+        this.updateReseller = updateReseller;
+        this.dateTimeService = dateTimeService;
+    }
+
+    async Task<UpdateResellerResponse> IUpdateReseller.UpdateResellerAsync(DataContext context, UpdateResellerRequest request)
+    {
+        var response = await this.updateReseller.UpdateResellerAsync(context, request);
+        if (!response.IsSuccessful)
+        {
+            return response;
+        }
+        var vendors = await context.Vendor.Where(x => x.IsActive).ToListAsync();
+        foreach (var vendor in vendors)
+        {
+            var siteInformation = await context.SiteInformation.SingleOrDefaultAsync(x => x.OrganizationId == response.Reseller.OrganizationId && x.VendorId == vendor.Id);
+            if (siteInformation != null)
+            {
+                continue;
+            }
+
+            siteInformation = new SiteInformation()
+            {
+                AccountId = string.Empty,
+                Balance = 0,
+                CreatedOn = this.dateTimeService.UtcNow,
+                Description = vendor.Name,
+                ItemId = string.Empty,
+                OrganizationId = response.Reseller.OrganizationId,
+                ResellerId = response.Reseller.Id,
+                VendorId = vendor.Id,
+                Url = string.Empty
+            };
+
+            context.SiteInformation.Add(siteInformation);
+        }
+
+        return response;
+    }
+}
